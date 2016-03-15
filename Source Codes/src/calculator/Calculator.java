a780c4a96d6d4da1419a14233fccbbcc576a0b35
Sukrit <Sukrit.adhikari@gmail.com>
2016-03-15 14:54:39 -0500

    backup commit

---
+++ Source Codes/src/calculator/Calculator.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author Sukrit
+ */
+public class Calculator  {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+         calculator.CalculatorForm calc=new calculator.CalculatorForm();
+          calc.setSize(380,310);
+          calc.setVisible(true);
+          calc.setResizable(false);
+                  
+    }
+}
